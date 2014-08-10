7c994b5f603560a425359953843292aeb16e2b2f
Mohammed Junaid <mdjunaid.54@gmail.com>
2014-08-09 20:43:19 -0700

    server.js cleanup

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 var fs = require('fs')
 var http = require('http')
-var strftime = require('strftime')
+//var strftime = require('strftime')
 
 var i = 0;
-var path = "/root/configure/";
-var xivelyBackendFile = path + "xively.txt"
-var keys = ["id", "description", "position", "ip", "rssi"]
 function send(res, header)
 {
     res.writeHead(200, "OK", header);
@@ -23,10 +20,9 @@ var server = http.createServer(function(req, res) {
 
     var flag = 0;
     var data = {}
-//  console.log(req.method);
-//  console.log(req.headers);
+    console.log(req.method);
+    console.log(req.headers);
     console.log("IP: " + req.connection.remoteAddress);
-    file = path + '/' + req.headers['id']
     if (req.method == 'GET') {
         console.log("GET Method");
         flag = 1;
@@ -56,12 +52,7 @@ var server = http.createServer(function(req, res) {
         console.log("POST Method "+ file);
         var data = {}
         parse_header(req.headers, data);
-        line = data["id"] + ',' + data["rssi"] + ',' + strftime('%Y-%m-%dT%H:%M:%S') + '-08:00\n';
-        //console.log(line);
-        fs.appendFile(xivelyBackendFile, line, function(err) {
-            if (err)
-                console.log("error while appendingFile");
-        });
+        console.log(data);
         send(res, data);
     }
-}).listen(80, '128.97.93.163');
+}).listen(8080, '128.97.93.163');
